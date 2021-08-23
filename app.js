0b2b62252039a1da21b533e77a3d81c4c611d6fd
KronosGR <geo.elgeo@gmail.com>
2021-08-23 17:51:34 +0200

    Added some html files and updated routes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 const adminRoutes = require("./routes/admin");
@@ -8,11 +9,11 @@ const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded( {extended: false}));
 
-app.use(adminRoutes);
+app.use('/admin/', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res) => {
-  res.send('<h1>Page not Found</h1>');
+  res.sendFile(path.join(__dirname, 'views','404.html'));
 })
 
 
--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,17 @@
 const express = require("express")
+const path = require("path")
+
 
 const router = express.Router();
 
+// GET /admin/add-product
 router.get('/add-product', (req, res, next)=>{
-  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+  res.sendFile(path.join(__dirname, '../', 'views', 'add-product-html'));
 });
 
-router.post('/product', (req, res, next)=>{
+
+// POST /admin/add-product
+router.post('/add-product', (req, res, next)=>{
   console.log(req.body)
   res.redirect('/');
 })
