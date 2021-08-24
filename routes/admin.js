524244faa96b75bd2267d4fe0067a66ecec808ee
KronosGR <geo.elgeo@gmail.com>
2021-08-24 17:40:36 +0200

    Fixed route bugs and added some style

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,14 @@
 const express = require("express")
-const path = require("path")
+const path = require("path");
+
+const rootDir = require("../utils/path");
 
 
 const router = express.Router();
 
 // GET /admin/add-product
 router.get('/add-product', (req, res, next)=>{
-  res.sendFile(path.join(__dirname, '../', 'views', 'add-product-html'));
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 
