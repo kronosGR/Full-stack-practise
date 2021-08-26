498e65c4104b270dd05f5104c68c1dee537b9d0f
KronosGR <geo.elgeo@gmail.com>
2021-08-26 08:41:54 +0200

    small changes before templates

--- routes/admin.js
+++ routes/admin.js
@@ -3,19 +3,23 @@ const path = require("path");
 
 const rootDir = require("../utils/path");
 
-
 const router = express.Router();
 
+const products = [];
+
 // GET /admin/add-product
-router.get('/add-product', (req, res, next)=>{
+router.get('/add-product', (req, res, next) => {
   res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 
 // POST /admin/add-product
-router.post('/add-product', (req, res, next)=>{
-  console.log(req.body)
+router.post('/add-product', (req, res, next) => {
+  products.push({
+    title: req.body.title
+  })
   res.redirect('/');
 })
 
-module.exports = router;
+module.exports.routes = router;
+module.exports.products = products;
