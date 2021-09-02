a7002d2f4e45599805e0bafce8c8bc03a9d88bdb
KronosGR <geo.elgeo@gmail.com>
2021-09-02 13:34:52 +0200

    Added more views and routes

--- controllers/error.js
+++ controllers/error.js
@@ -1,6 +1,6 @@
 exports.get404 = (req, res) => {
     res.render('404', {
         pageTitle: 'Page not found',
-        path:''
+        path:'/404'
     })
 }
--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,19 @@
 const express = require("express")
 const path = require("path");
 
-const productsController = require("../controllers/products");
+const adminController = require("../controllers/admin");
 
 const router = express.Router();
 
 
 // GET /admin/add-product
-router.get('/add-product', productsController.getAddProduct);
-
+router.get('/add-product', adminController.getAddProduct);
 
 // POST /admin/add-product
-router.post('/add-product', productsController.postAddProduct);
+router.post('/add-product', adminController.postAddProduct);
+
+router.get('/products', adminController.getProducts);
+
+
 
 module.exports = router;
