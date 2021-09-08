48c2c1ea0f0fc4eff3266c4f7766524ebfe6082e
KronosGR <geo.elgeo@gmail.com>
2021-09-08 11:54:09 +0200

    Added delete and edit product in the controller

--- controllers/admin.js
+++ controllers/admin.js
@@ -14,13 +14,13 @@ exports.postAddProduct = (req, res, next) => {
     const price = req.body.price;
     const description = req.body.description;
 
-    const product = new Product(title, imageUrl, description, price);
+    const product = new Product(null, title, imageUrl, description, price);
     product.save();
     res.redirect('/');
 }
 
 exports.getEditProduct = (req, res, next) => {
-    const editMode = req.query.edit; 
+    const editMode = req.query.edit;
     if (!editMode) {
         return res.redirect('/');
     }
@@ -39,7 +39,14 @@ exports.getEditProduct = (req, res, next) => {
 }
 
 exports.postEditProduct = (req, res, next) => {
-    
+    const prodId = req.body.productId;
+    const updatedTitle = req.body.title;
+    const updatedPrice = req.body.price;
+    const updatedImageUrl = req.body.imageUrl;
+    const updatedDesc = req.body.description;
+    const updatedProduct = new Product(prodId, updatedTitle, updatedImageUrl, updatedDesc, updatedPrice);
+    updatedProduct.save();
+    res.redirect('/admin/products');
 }
 
 exports.getProducts = (req, res, next) => {
@@ -52,3 +59,7 @@ exports.getProducts = (req, res, next) => {
     });
 }
 
+exports.postDeleteProduct = (req, res, next) => {
+    const prodId = req.body.productId;
+
+}
