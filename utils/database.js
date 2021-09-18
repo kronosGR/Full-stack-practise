b70aa68b9cb2bb305da5efed56ea73999816ebe6
KronosGR <geo.elgeo@gmail.com>
2021-09-18 11:44:35 +0200

    Initialized sequelize

--- utils/database.js
+++ utils/database.js
@@ -1,12 +1,22 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-	host: '172.21.192.1',
-	user: 'root',
-	database: 'node_complete',
-	password: '123456',
+// const pool = mysql.createPool({
+// 	host: '172.21.192.1',
+// 	user: 'root',
+// 	database: 'node_complete',
+// 	password: '123456',
+// });
+
+
+// module.exports = pool.promise();
+
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('node-complete', 'root', 'node-complete', {
+	dialect:'mysql',
+	host: '172.21.192.1'
 });
 
 
-module.exports = pool.promise();
+module.exports = sequelize;
 
