0565715b43397cf9966a1f858ced1a98c63c6747
KronosGR <geo.elgeo@gmail.com>
2021-09-27 13:39:24 +0200

    Add products to the cart table

--- app.js
+++ app.js
@@ -64,6 +64,9 @@ sequelize
   })
   .then((user) => {
     // console.log(user)
+    return user.createCart();
+  })
+  .then((cart) => {
     app.listen(3000);
   })
   .catch((err) => {
--- controllers/shop.js
+++ controllers/shop.js
@@ -46,35 +46,56 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  Cart.getCart((cart) => {
-    Product.fetchAll((product) => {
-      const cartProducts = [];
-      for (product of product) {
-        const cartProductData = cart.products.find(
-          (prod) => prod.id === product.id
-        );
-        if (cart.products.find((prod) => prod.id === product.id)) {
-          cartProducts.push({
-            productData: product,
-            qty: cartProductData.qty,
+  req.user
+    .getCart()
+    .then((cart) => {
+      return cart
+        .getProducts()
+        .then((products) => {
+          res.render('shop/cart', {
+            path: '/cart',
+            pageTitle: 'Your Cart',
+            products: cartProducts,
           });
-        }
-      }
-      res.render('shop/cart', {
-        path: '/cart',
-        pageTitle: 'Your Cart',
-        products: cartProducts,
-      });
-    });
-  });
+        })
+        .catch((err) => console.log(err));
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId, (product) => {
-    Cart.AddProduct(prodId, product.price);
-  });
-  res.redirect('/cart');
+
+  // declare a cart object to be available to the the arrow function below.
+  let fetchedCart;
+
+  req.user
+    .getCart()
+    .then((cart) => {
+      fetchedCart = cart;
+      return cart.getProducts({ where: { id: prodId } });
+    })
+    .then((products) => {
+      if (products.length > 0) {
+        const product = products[0];
+      }
+      let newQuantity = 1;
+      if (product) {
+        // TODO
+      } else {
+        return Product.findByPk(prodId)
+          .then((product) => {
+            return fetchedCart.addProduct(product, {
+              through: { quantity: newQuantity },
+            });
+          })
+          .catch((err) => console.log(err));
+      }
+    })
+    .then(() => {
+      res.redirect('/cart');
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
