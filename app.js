04327b885929cc65a22862ca5631f2835f8d99f5
KronosGR <geo.elgeo@gmail.com>
2021-09-30 11:16:13 +0200

    First changes for MongoDB

--- app.js
+++ app.js
@@ -8,75 +8,37 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+// const adminRoutes = require('./routes/admin');
+// const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 const errorController = require('./controllers/error');
-const sequelize = require('./utils/database');
-const Product = require('./models/product');
-const User = require('./models/user');
-const Cart = require('./models/cart');
-const CartItem = require('./models/cart-item');
-const Order = require('./models/order');
-const OrderItem = require('./models/order-item');
+const mongoConnect = require('./utils/database');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 // get the dummy user for epxiremental use
 app.use((req, res, next) => {
-  User.findByPk(1)
-    .then((user) => {
-      // store the user to the req for future use
-      req.user = user;
-      next();
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  // User.findByPk(1)
+  //   .then((user) => {
+  //     // store the user to the req for future use
+  //     req.user = user;
+  //     next();
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
 });
 
-app.use('/admin/', adminRoutes);
-app.use(shopRoutes);
+// app.use('/admin/', adminRoutes);
+// app.use(shopRoutes);
 
 app.use(errorController.get404);
-
-// declares the user has many products and onDelete delete in depth
-Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
-// not important to have
-User.hasMany(Product);
-User.hasOne(Cart);
-Cart.belongsTo(User);
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Product, { through: OrderItem })
-
-// creates tables for all the defined models
-sequelize
-  //.sync({ force: true }) //forces to overwrite the table, not good for production
-  .sync()
-  .then((result) => {
-    return User.findByPk(1);
-  })
-  .then((user) => {
-    if (!user) {
-      return User.create({ name: 'kronos', email: 'test@test.com' });
-    }
-    return Promise.resolve(user);
-  })
-  .then((user) => {
-    // console.log(user)
-    return user.createCart();
-  })
-  .then((cart) => {
-    app.listen(3000);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-
 // install 3 template engines
 // npm install --save ejs pug express-handlebars
+
+mongoConnect((client) => {
+  console.log(client);
+  app.listen(3000);
+});
--- utils/database.js
+++ utils/database.js
@@ -5,18 +5,33 @@
 // 	user: 'root',
 // 	database: 'node_complete',
 // 	password: '123456',
+//
 // });
-
 
 // module.exports = pool.promise();
 
-const Sequelize = require('sequelize');
+// sequelize
+// const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node_complete', 'root', '123456', {
-	dialect:'mysql',
-	host: 'localhost'
-});
+// const sequelize = new Sequelize('node_complete', 'root', '123456', {
+// 	dialect:'mysql',
+// 	host: 'localhost'
+// });
 
+// module.exports = sequelize;
 
-module.exports = sequelize;
+const mongodb = require('mongodb');
 
+const mongoConnect = (callback) => {
+  const MongoClient = mongodb.MongoClient;
+  MongoClient.connect(
+    'mongodb+srv://kronos:yxhI2XOMH63PzHrm@cluster0.hrnez.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+  )
+    .then((client) => {
+      console.log('Connected!');
+			callback(client);
+    })
+    .catch((err) => console.log(err));
+};
+
+module.exports = mongoConnect;
