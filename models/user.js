4c7ca775107a22215fd460fa04c916e176c5e150
KronosGR <geo.elgeo@gmail.com>
2021-10-05 19:27:59 +0200

    Display cart and delete cart items

--- models/user.js
+++ models/user.js
@@ -44,6 +44,38 @@ class User {
       );
   }
 
+  getCart() {
+    const db = getDb();
+    const productIds = this.cart.items.map((i) => i.productId);
+    return db
+      .collection('products')
+      .find({ _id: { $in: productIds } })
+      .toArray()
+      .then((products) => {
+        return products.map((product) => {
+          return {
+            ...product,
+            quantity: this.cart.items.find(
+              (i) => i.productId.toString() === product._id.toString()
+            ).quantity,
+          };
+        });
+      });
+  }
+
+  deleteItemFromCart(productId) {
+    const updatedCartItems = this.cart.items.filter((item) => {
+      return item.productId.toString() !== productId.toString();
+    });
+    const db = getDb();
+    return db
+      .collection('users')
+      .updateOne(
+        { _id: new ObjectId(this._id) },
+        { $set: { cart: {items :updatedCartItems } }}
+      );
+  }
+
   static findById(userId) {
     const db = getDb();
     return db
