1e72a62373fdd64430fd0b52657027f2a7577325
Katsanakis Georgios <geo.elgeo@gmail.com>
2021-10-18 11:10:06 +0200

    Added Logout

--- controllers/auth.js
+++ controllers/auth.js
@@ -20,7 +20,7 @@ exports.postLogin = (req, res, next) => {
 
 exports.postLogout = (req, res, next) => {
   req.session.destroy(err => {
-    console.log(err);
+      console.log(err);
     res.redirect('/');
   });
 };
