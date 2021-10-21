c6f1eaf7fcfe4901a46fece9535dbf1e4a5e206f
Georgios Katsanakis <geo.elgeo@gmail.com>
2021-10-21 08:39:37 +0200

    added csrf protection

--- app.js
+++ app.js
@@ -6,68 +6,68 @@ const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
 
-const MONGODB_URI =
-  'mongodb+srv://kronos:yxhI2XOMH63PzHrm@cluster0.hrnez.mongodb.net/Shop?w=majority';
-
-const app = express();
-const store = new MongoDBStore({
-  uri: MONGODB_URI,
-  collection: 'sessions',
-});
-
-const csrfProtection = csrf();
-
-// EJS setup, and it does not support layouts
-app.set('view engine', 'ejs');
-app.set('views', 'views');
-
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-const authRoutes = require('./routes/auth');
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(
-  session({
-    secret: 'my secret',
-    resave: false,
-    saveUninitialized: false,
-    store: store,
-  })
-);
-
-app.use(csrfProtection);
-
-app.use((req,res,next) => {
-  if (!req.session.user){
-    return next();
-  }
-  User.findById(req.session.user._id)
-  .then(user => {
-    req.user = user;
-    next();
-  })
-  .catch(err => console.log(err));
-})
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
-const { Session } = require('inspector');
-const { nextTick } = require('process');
 
+const MONGODB_URI =
+  'mongodb+srv://kronos:yxhI2XOMH63PzHrm@cluster0.hrnez.mongodb.net/Shop?w=majority';
 
-app.use('/admin/', adminRoutes);
-app.use(shopRoutes);
-app.use(authRoutes);
-
-app.use(errorController.get404);
-// install 3 template engines
-// npm install --save ejs pug express-handlebars
-
-mongoose
-  .connect(MONGODB_URI)
-  .then((result) => {
-    
-    app.listen(3000);
-  })
-  .catch((err) => console.log(err));
+  const app = express();
+  const store = new MongoDBStore({
+    uri: MONGODB_URI,
+    collection: 'sessions'
+  });
+  const csrfProtection = csrf();
+  
+  app.set('view engine', 'ejs');
+  app.set('views', 'views');
+  
+  const adminRoutes = require('./routes/admin');
+  const shopRoutes = require('./routes/shop');
+  const authRoutes = require('./routes/auth');
+  
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(
+    session({
+      secret: 'my secret',
+      resave: false,
+      saveUninitialized: false,
+      store: store
+    })
+  );
+  app.use(csrfProtection);
+  
+  app.use((req, res, next) => {
+    if (!req.session.user) {
+      return next();
+    }
+    User.findById(req.session.user._id)
+      .then(user => {
+        req.user = user;
+        next();
+      })
+      .catch(err => console.log(err));
+  });
+  
+  app.use((req, res, next) => {
+    res.locals.isAuthenticated = req.session.isLoggedIn;
+    res.locals.csrfToken = req.csrfToken();
+    next();
+  });
+  
+  app.use('/admin', adminRoutes);
+  app.use(shopRoutes);
+  app.use(authRoutes);
+  
+  app.use(errorController.get404);
+  
+  mongoose
+    .connect(MONGODB_URI)
+    .then(result => {
+      app.listen(3000);
+    })
+    .catch(err => {
+      console.log(err);
+    });
