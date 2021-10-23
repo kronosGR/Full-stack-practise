43b1fde3590271a70b88388c6ef7e57d7cb9812a
Georgios Katsanakis <geo.elgeo@gmail.com>
2021-10-23 07:27:25 +0200

    Added reset password token and send email

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 const sendgridTransport = require('nodemailer-sendgrid-transport');
@@ -82,10 +83,7 @@ exports.postSignup = (req, res, next) => {
   User.findOne({ email: email })
     .then((userDoc) => {
       if (userDoc) {
-        req.flash(
-          'error',
-          'Email exists already, please enter a different one'
-        );
+        req.flash('error', 'Email exists already, please enter a different one');
         return res.redirect('./signup');
       }
       return bcrypt.hash(password, 12).then((hashedPassword) => {
@@ -118,16 +116,51 @@ exports.postLogout = (req, res, next) => {
   });
 };
 
-exports.getReset = (req,res, next) =>{
+exports.getReset = (req, res, next) => {
   let message = req.flash('error');
-  if (message.length>0){
-    message = message[0]
+  if (message.length > 0) {
+    message = message[0];
   } else {
-    message=null;
+    message = null;
   }
   res.render('auth/reset', {
     path: '/reset',
     pageTitle: 'Reset Password',
-    errorMessage: message
-  })
-}
+    errorMessage: message,
+  });
+};
+
+exports.postReset = (req, res, next) => {
+  crypto.randomBytes(32, (err, buffer) => {
+    if (err) {
+      console.log(err);
+      return res.redirect('/reset');
+    }
+    const token = buffer.toString('hex');
+    User.findOne({ email: req.body.email })
+      .then((user) => {
+        if (!user) {
+          req.flash('error', 'No account with that email found');
+          return res.redirect('/reset');
+        }
+        user.resetToken = token;
+        user.resetTokenExpiration = Date.now() + 3600;
+        return user.save();
+      })
+      .then((result) => {
+        res.redirect('/');
+        transporter.sendMail({
+          to: req.body.email,
+          from: 'geo.elgeo@gmail.com',
+          subject: 'Password Reset',
+          html: `
+        <p>You have requested password reset</p>
+        <p>Click this <a href="http://localhost:3000/reset/${token}">link</a> to set a new password</p>
+        `,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+};
