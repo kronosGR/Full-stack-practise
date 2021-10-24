1c578cd574011166be0822425409042f5432adf6
KronosGR <geo.elgeo@gmail.com>
2021-10-24 09:09:22 +0200

    update with new password

--- routes/auth.js
+++ routes/auth.js
@@ -17,4 +17,5 @@ router.post('/logout', authController.postLogout);
 router.get('/reset', authController.getReset);
 router.post('/reset',authController.postReset);
 router.get('/reset/:token', authController.getNewPassword);
+router.post('/new-password', authController.postNewPassword);
 module.exports = router;
