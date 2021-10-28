a75936aa115b4f738382a54aa947da8e338a6d34
KronosGR <geo.elgeo@gmail.com>
2021-10-28 09:44:42 +0200

    Added 500 view

--- app.js
+++ app.js
@@ -48,10 +48,13 @@ const MONGODB_URI =
     }
     User.findById(req.session.user._id)
       .then(user => {
+        if (!user) return next();
         req.user = user;
         next();
       })
-      .catch(err => console.log(err));
+      .catch(err => {
+        throw new Error(err)
+      });
   });
   
   app.use((req, res, next) => {
@@ -64,7 +67,9 @@ const MONGODB_URI =
   app.use(shopRoutes);
   app.use(authRoutes);
   
+  app.use('/500', errorController.get500);
   app.use(errorController.get404);
+  
   
   mongoose
     .connect(MONGODB_URI)
