feef2a5278435985d358ba84445c2bb52aa7e10a
KronosGR <geo.elgeo@gmail.com>
2021-10-31 08:32:29 +0100

    Added page pagination to getIndex

--- controllers/shop.js
+++ controllers/shop.js
@@ -5,6 +5,8 @@ const PDFDocument = require('pdfkit');
 const Product = require('../models/product');
 const Order = require('../models/order');
 
+const ITEMS_PER_PAGE = 2;
+
 exports.getProducts = (req, res, next) => {
   Product.find()
     .then((products) => {
@@ -40,12 +42,28 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
+  const page = req.query.page || 1;
+  let totalItems;
+
   Product.find()
+    .count()
+    .then((numProducts) => {
+      totalItems = numProducts;
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
     .then((products) => {
       res.render('shop/index', {
         prods: products,
         pageTitle: 'Shop',
         path: '/',
+        totalProducts: totalItems,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Match.ceil(totalItems / ITEMS_PER_PAGE),
       });
     })
     .catch((err) => {
@@ -171,12 +189,11 @@ exports.getInvoice = (req, res, next) => {
       let totalPrice = 0;
       order.products.forEach((prod) => {
         totalPrice += prod.quantity * prod.product.price;
-        pdfDoc.fontSize(14)
-        .text(
-          prod.product.title + ' - ' + prod.quantity + ' x $' + prod.product.price
-        );
+        pdfDoc
+          .fontSize(14)
+          .text(prod.product.title + ' - ' + prod.quantity + ' x $' + prod.product.price);
       });
-      pdfDoc.text('---')
+      pdfDoc.text('---');
       pdfDoc.fontSize(20).text('Total Price: $' + totalPrice);
 
       pdfDoc.end();
